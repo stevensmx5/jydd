9646330e33aa4d74143fdf4b937046fd5b7a35c0
stevensmx5 <sunmaoxin1986221@163.com>
2017-07-23 16:28:25 +0800

    index页面

---
+++ js/JScript.js
@@ -0,0 +1,10 @@
+﻿function move_top() {
+    $("html, body").scrollTop(0);
+}
+function news_img(a) {
+    var id = $(a).attr("id"),
+        src = "../images/" + id + ".png";
+    $(a).siblings().removeClass("curr");
+    $(a).addClass("curr");
+    $(".o-img").find("img").attr("src", src);
+}
